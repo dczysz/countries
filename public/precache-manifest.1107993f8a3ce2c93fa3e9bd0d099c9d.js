d1986d5a48ba7d627b13929f5b27a6ca8de2f9ae
D. Czysz <=>
2019-09-09 12:18:03 -0600

    Frontend build added

---
+++ public/precache-manifest.1107993f8a3ce2c93fa3e9bd0d099c9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac438766f26478ab25c698b968c492f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ccc8ca5335668791d73",
+    "url": "/static/css/main.34c1850e.chunk.css"
+  },
+  {
+    "revision": "413f09014d4f36d96b52",
+    "url": "/static/js/2.53ae8cb5.chunk.js"
+  },
+  {
+    "revision": "3ccc8ca5335668791d73",
+    "url": "/static/js/main.e697f5d6.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "a617b62cd8089465865a37c676da00e2",
+    "url": "/static/media/LouisGeorgeCafe.a617b62c.ttf"
+  },
+  {
+    "revision": "cc84ead9ce361c9219bb7f22b65fab74",
+    "url": "/static/media/worldmap.cc84ead9.svg"
+  }
+]);
